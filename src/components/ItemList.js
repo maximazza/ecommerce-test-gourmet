37a0b26219150e9a8e175fe8e447fa6447d3c3d8
mazza gonzalez maximiliano <maximazza22@gmail.com>
2022-08-09 19:30:54 -0300

    trabajando codigo

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState} from 'react';
-//import {useParams} from 'react';
+import{useParams} from 'react-router-dom';
 import ItemProduct from "./ItemProduct/ItemProduct";
 import { productList } from '../data/data.js';
 
 const ItemList = () => {
- //const{categoryId} = useParams();
+  const{categoryId} = useParams();
 
   const [products, setProductsFromDB] = useState([]);
 
-  //const filterByCategory = productList.filter((productList) => productList.category === categoryId);
-  const getProducts= new Promise((resolve, reject) => {
+  const filterByCategory = productList.filter((productList) => productList.category === categoryId);
+  const getProductsFromDB = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(true){
+      if(categoryId){
+        resolve(filterByCategory)
+      }else{
         resolve(productList);
-        } else{
-          reject();
         }
       }, 200);
     });
 
-    const getProductsFromDB = async () => {
-      try {
-        const result = await getProducts;
-        setProductsFromDB(result);
-      } catch (error) {
-        console.error(error);
-        alert('No podemos mostrar los productos en este momento');
-      }
-    };
-    useEffect(() => {
-      getProductsFromDB();
-    }, []);
+  useEffect(() => {
+    getProductsFromDB
+    .then((res)=>{
+      setProductsFromDB(res);
+    })
+    .catch((error)=>{
+      console.log('fallo la llamada');
+    })
+    .finally(()=>{
+    });
+
+  }, []); 
 
   return (
     <div className="product-list-container">
